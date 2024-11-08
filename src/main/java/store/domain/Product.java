ac851edc520019006212c29843bb676a23c42222
dltmddus <tmddus4671@gmail.com>
2024-11-08 23:49:02 +0900

    refactor, feat(Product) : 리팩토링, 상품 수량 가져오는 메서드 추가
    double 타입 price필드 int로 변경

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class Product {
     private final String name;
-    private final double price;
+    private final int price;
     private final List<Promotion> promotions = new ArrayList<>();
 
-    public Product(String name, double price) {
+    public Product(String name, int price) {
         this.name = name;
         this.price = price;
     }
@@ -17,15 +17,17 @@ public class Product {
         return name;
     }
 
-    public double getPrice() {
-        return price;
-    }
-
     public List<Promotion> getPromotions() {
         return promotions;
     }
 
     public void addPromotion(Promotion promotion) {
         promotions.add(promotion);
+    }
+
+    public int getAllQuantityAboutProduct() {
+        return promotions.stream()
+                .mapToInt(Promotion::getQuantity)
+                .sum();
     }
 }
