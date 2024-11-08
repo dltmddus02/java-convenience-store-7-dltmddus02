c42f3e5f8e17c72ca2a47ead38e0e9223f38e0c7
dltmddus <tmddus4671@gmail.com>
2024-11-08 23:46:59 +0900

    reefactor(InputErrorMessage, InputException) : view.input.exception으로 패키지 이동

---
+++ src/main/java/store/view/input/exception/InputException.java
@@ -0,0 +1,8 @@
+package store.view.input.exception;
+
+public class InputException extends IllegalArgumentException {
+
+    public InputException(InputErrorMessage errorMessage) {
+        super(errorMessage.getMessage());
+    }
+}
