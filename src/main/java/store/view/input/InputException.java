d1238505834e4a2cdb5aebf05bb6734c3ef91f79
dltmddus <tmddus4671@gmail.com>
2024-11-09 00:04:25 +0900

    refactor(InputErrorMessage, InputException) : 클래스 패키지 이동에 의한 삭제

--- src/main/java/store/view/input/InputException.java
+++
@@ -1,8 +0,0 @@
-package store.view.input;
-
-public class InputException extends IllegalArgumentException{
-
-    public InputException(InputErrorMessage errorMessage) {
-        super(errorMessage.getMessage());
-    }
-}
