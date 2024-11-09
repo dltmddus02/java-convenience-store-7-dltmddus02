6927f8e79ddbaf2b81396dd1f021aa6f5ea9d4db
dltmddus <tmddus4671@gmail.com>
2024-11-09 21:48:57 +0900

    feat(Promotion) : Promotion 클래스 추가

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,23 +1,37 @@
 package store.domain;
 
 public class Promotion {
-    private int quantity;
     private final String promotionType;
+    private int buy;
+    private int get;
+    private String startDate;
+    private String endDate;
 
-    public Promotion(int quantity, String promotionType) {
-        this.quantity = quantity;
+    public Promotion(String promotionType, int buy, int get, String startDate, String endDate) {
         this.promotionType = promotionType;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int newQuantity) {
-        quantity = newQuantity;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 
     public String getPromotionType() {
         return promotionType;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
     }
 }
