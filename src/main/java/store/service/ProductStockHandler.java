bc27ffdc67be68a045a994a7da08d3fe046bf759
dltmddus <tmddus4671@gmail.com>
2024-11-11 14:37:04 +0900

    fix(ProductStockHandler) : 프로모션 관련 무료증정, 재고부족 시에 로직 수정

--- src/main/java/store/service/ProductStockHandler.java
+++ src/main/java/store/service/ProductStockHandler.java
@@ -3,7 +3,10 @@ package store.service;
 import camp.nextstep.edu.missionutils.DateTimes;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import store.domain.Product;
+import store.domain.Receipt;
 import store.domain.Stock;
 import store.repository.ProductRepository;
 import store.view.input.InputView;
@@ -16,35 +19,49 @@ public class ProductStockHandler {
 
     private final ProductRepository productRepository;
     private final ProductService productService;
+    private Receipt receipt;
+    private int freeQuantity = 0;
+    private Map<String, Integer> finalChoosing = new HashMap<>();
 
-    public ProductStockHandler(ProductRepository productRepository, ProductService productService) {
+    public ProductStockHandler(ProductRepository productRepository, ProductService productService, Receipt receipt) {
         this.productRepository = productRepository;
         this.productService = productService;
+        this.receipt = receipt;
+    }
+
+    public void updateReceipt(Receipt receipt) {
+        this.receipt = receipt;
     }
 
     public void deductStock(String productName, int requestedQuantity) {
+        freeQuantity = 0;
         validateExistingProduct(productName);
 
         int neededQuantity = requestedQuantity;
         Product product = productRepository.findByName(productName).get();
         validateSufficientStock(product, requestedQuantity);
 
-        for (Stock stock : product.getStocks()) {
-            if (isPromotionOngoing(stock)) {
+        receipt.addPurchasedItem(productName, requestedQuantity, product.getPrice()); // 구매한 상품 이름과 개수 (영수증)
+
+        for (Stock stock : product.getStocks()) { // 프로모션 2개 돌면서 확인한다.
+            if (isPromotionOngoing(stock)) { // 프로모션 기간이라면?
                 neededQuantity = validateRemainingPromotionStock(stock, product.getName(), requestedQuantity);
-            }
+            } // neededQuantity에 최종적으로 구매할 물건 개수 들어간다. 0개일 수도 있다.
 
             int quantity = stock.getQuantity();
-            if (quantity >= neededQuantity) {
-                int newQuantity = quantity - neededQuantity;
+            if (quantity >= neededQuantity) { // 해당 프로모션의 재고가 내가 구매할 물건보다 많으면 구매 가능함
+                int newQuantity = quantity - neededQuantity; // 내가 구매하고 남은 재고 newQuantity
                 stock.setQuantity(newQuantity);
                 updatePromotionStockInFile(product.getName(), stock.getPromotionType(), newQuantity);
                 break;
             }
-            neededQuantity -= quantity;
+            neededQuantity -= quantity; // 해당 프로모션의 재고 부족하면 일단 그 프로모션거는 다 쓰고 남은거 다음 프로모션으로 돌리기
             stock.setQuantity(0);
             updatePromotionStockInFile(product.getName(), stock.getPromotionType(), 0);
         }
+        receipt.setFreeQuantityForItem(productName, freeQuantity); // 무료로 증정하는 상품과 개수 (영수증)
+//        receipt.addEventDiscount(freeQuantity * product.getPrice()); // ?
+//        finalChoosing.put(productName, requestedQuantity);
     }
 
     private void validateExistingProduct(String name) {
@@ -88,18 +105,22 @@ public class ProductStockHandler {
             while (requestedQuantity >= totalQuantityUnit && remainingQuantity >= totalQuantityUnit) {
                 requestedQuantity -= totalQuantityUnit;
                 remainingQuantity -= totalQuantityUnit;
+                freeQuantity++; // 무료로 증정되는 개수 추가
+//                receipt.addEventDiscount(productRepository.findByName(name).get().getPrice());
             }
 
             if (isFreeProductOffer(stock, remainingQuantity, requestedQuantity)) {
                 return returnQuantity + calculateFreeProduct(stock, name);
             }
-            return calculatePromotionDiscountNotApplied(remainingQuantity, requestedQuantity, name);
+            if (isPromotionDiscountNotApplied(remainingQuantity, requestedQuantity)) {
+                returnQuantity = calculatePromotionDiscountNotApplied(returnQuantity, requestedQuantity, name);
+            }
+            return returnQuantity;
         } catch (InputException e) {
             System.out.println(e.getMessage());
         }
         return returnQuantity;
     }
-
 
     private boolean isFreeProductOffer(Stock stock, int remainingQuantity, int requestedQuantity) {
         return remainingQuantity > requestedQuantity && requestedQuantity == stock.getPromotion().getBuy();
@@ -109,23 +130,47 @@ public class ProductStockHandler {
         int getQuantity = stock.getPromotion().getGet();
 
         System.out.printf(PROMOTION_FREE_PRODUCT_OFFER, name, getQuantity);
-        String input = InputView.inputYesOrNo();
+        String input;
+        try {
+            input = InputView.inputYesOrNo();
+        } catch (InputException e) {
+            System.out.println(e.getMessage());
+            return calculateFreeProduct(stock, name); // 예외 발생 시 다시 호출하여 입력 재시도
+        }
         if (input.equals("Y")) {
+            freeQuantity++; // 하나 무료로 더 가져와야되니까 증정 추가
+            receipt.addQuantityForItem(name, getQuantity); // 하나 무료로 더 가져와야 하니까 최종 구매하는 개수 하나더 추가 (영수증)
+//            receipt.addEventDiscount(productRepository.findByName(name).get().getPrice());
             return getQuantity;
         }
         return 0;
 
     }
 
-    private int calculatePromotionDiscountNotApplied(int remainingQuantity, int requestedQuantity, String name) {
-        if (remainingQuantity < requestedQuantity) {
-            System.out.printf(PROMOTION_DISCOUNT_NOT_APPLIED, name, requestedQuantity);
-            String input = InputView.inputYesOrNo();
-            if (input.equals("N")) {
-                return 0;
-            }
+    private boolean isPromotionDiscountNotApplied(int remainingQuantity, int requestedQuantity) {
+        return remainingQuantity < requestedQuantity;
+    }
+
+
+    private int calculatePromotionDiscountNotApplied(int returnQuantity, int requestedQuantity, String name) {
+//        if (remainingQuantity < requestedQuantity) {
+        System.out.printf(PROMOTION_DISCOUNT_NOT_APPLIED, name, requestedQuantity);
+        String input;
+        try {
+            input = InputView.inputYesOrNo();
+        } catch (InputException e) {
+            System.out.println(e.getMessage());
+            return calculatePromotionDiscountNotApplied(returnQuantity, requestedQuantity, name);
         }
-        return requestedQuantity;
+        if (input.equals("N")) {
+            receipt.setQuantityForItem(name, 0);
+            receipt.removePurchasedItem(name);
+            finalChoosing.remove(name);
+            return 0;
+        }
+        return returnQuantity;
+//        }
+//        return requestedQuantity;
     }
 
     private void updatePromotionStockInFile(String productName, String promotionType, int newQuantity) {
